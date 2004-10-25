cab17a67b11059c3134dec691e8c1e4eeafbccde
jobi <jobi@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2004-10-25 15:33:56 +0000

    forgot to add the files
    git-svn-id: svn://svn.minisip.org/minisip/trunk@890 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmsip/include/libmsip/SipHeaderWarning.h
@@ -0,0 +1,76 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/* Copyright (C) 2004 
+ *
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+/* Name
+ * 	SipHeaderWarning.h
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 
+*/
+
+#ifndef SIPHEADERWARNING_H
+#define SIPHEADERWARNING_H
+
+#include<libmsip/SipHeader.h>
+#include<stdint.h>
+
+/**
+ * @author Erik Eliasson
+*/
+
+class SipHeaderWarning: public SipHeader{
+	public:
+		SipHeaderWarning();
+		SipHeaderWarning(const string &build_from);
+
+		virtual ~SipHeaderWarning();
+
+                virtual std::string getMemObjectType(){return "SipHeaderWarning";}
+		
+		/**
+		 * returns string representation of the header
+		 */
+		string getString(); 
+
+		/**
+		 * @return The IP address of the contact header.
+		 */
+		string getWarning();
+		
+		void setWarning(const string &ua);
+
+		uint16_t getErrorCode();
+
+		void setErrorCode(const uint16_t &errorCodec);
+
+		string getDomainName();
+
+		void setDomainName(const string &domainName);
+		
+	private:
+		string warning;
+		uint16_t errorCode;
+		string domainName;
+};
+
+#endif
---
+++ libmsip/source/headers/SipHeaderWarning.cxx
@@ -0,0 +1,100 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/* Copyright (C) 2004 
+ *
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+/* Name
+ * 	SipHeaderWarning.cxx
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 
+*/
+
+#include<config.h>
+
+
+#include<libmsip/SipHeaderWarning.h>
+
+#include<libmutil/itoa.h>
+
+SipHeaderWarning::SipHeaderWarning(const string &build_from)
+		: SipHeader(SIP_HEADER_TYPE_WARNING)
+{
+	size_t blank1Pos = build_from.find(" ");
+	size_t blank2Pos, blank3Pos;
+	if (blank1Pos==string::npos){
+		return;
+	}
+	blank2Pos = build_from.substr(blank1Pos,string::npos).find(" ");
+	if (blank2Pos==string::npos){
+		return;
+	}
+	blank3Pos = build_from.substr(blank2Pos,string::npos).find(" ");
+	if (blank3Pos==string::npos){
+		return;
+	}
+
+	errorCode=atoi(build_from.substr(blank1Pos+1, 3).c_str());
+	domainName=build_from.substr(blank2Pos+1, blank3Pos-blank2Pos);
+	warning=build_from.substr(blank3Pos+2, build_from.size()-blank3Pos-1);
+
+}
+
+
+SipHeaderWarning::SipHeaderWarning()
+		: SipHeader(SIP_HEADER_TYPE_WARNING)
+{
+	errorCode=0;
+	domainName="";
+	warning="";
+}
+
+SipHeaderWarning::~SipHeaderWarning(){
+
+}
+
+string SipHeaderWarning::getString(){
+	return "Warning: "+itoa(errorCode)+" "+domainName+" \""+warning+"\"";
+}
+
+string SipHeaderWarning::getWarning(){
+	return warning;
+}
+		
+void SipHeaderWarning::setWarning(const string &warning){
+	this->warning=warning;
+}
+
+string SipHeaderWarning::getDomainName(){
+	return domainName;
+}
+
+void SipHeaderWarning::setDomainName(const string &domainName){
+	this->domainName=domainName;
+}
+
+uint16_t SipHeaderWarning::getErrorCode(){
+	return errorCode;
+}
+
+void SipHeaderWarning::setErrorCode(const uint16_t& errorCode){
+	this->errorCode=errorCode;
+}
