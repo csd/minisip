84ff1a06a61c1152fa48be45b50e5c004b6d3cbe
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2004-11-23 13:57:17 +0000

    * minisip: Added missing file.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@930 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ minisip/sip/PresenceMessageContent.cxx
@@ -0,0 +1,43 @@
+
+#include"PresenceMessageContent.h"
+
+MRef<SipMessageContent*> presenceSipMessageContentFactory(const string &buf){
+	        return new PresenceMessageContent(buf);
+}
+
+
+PresenceMessageContent::PresenceMessageContent(string from, string to, string os, string osd) : 
+		fromUri(from),
+		toUri(to),
+		onlineStatus(os), 
+		onlineStatusDesc(osd)
+{
+	string status;
+	if (os=="online"){
+		status="open";
+	}else{
+		status="inuse";
+	}
+	document = "<?xml version=\"1.0\"?>\n"
+                "<!DOCTYPE presence\n"
+                "PUBLIC \"-//IETF//DTD RFCxxxx XPIDF 1.0//EN\" \"xpidf.dtd\">\n"
+                "<presence>\n"
+                "  <presentity uri=\"" +toUri+";method=SUBSCRIBE\" />\n"
+                "  <atom id=\"1000\">\n"
+                "    <address uri=\""+fromUri+"\">\n"
+                "      <status status=\""+status+"\" />\n" 			//"open"|("closed")|"inuse"
+                "      <msnsubstatus substatus=\""+onlineStatus+"\" />\n"
+                "    </address>\n"
+                "  </atom>\n"
+                "</presence>\n";
+}
+
+PresenceMessageContent::PresenceMessageContent(const string &buildFrom){
+	document = buildFrom;
+}
+
+string PresenceMessageContent::getString(){
+	return document;
+}
+
+
---
+++ minisip/sip/PresenceMessageContent.h
@@ -0,0 +1,31 @@
+#ifndef _PRESENCEMESSAGECONTENT_H
+#define _PRESENCEMESSAGECONTENT_H
+
+#include<libmutil/MemObject.h>
+#include<libmsip/SipMessageContent.h>
+#include<libmsip/SipMessageContentFactory.h>
+
+
+
+MRef<SipMessageContent*> presenceSipMessageContentFactory(const string &);
+
+class PresenceMessageContent : public SipMessageContent{
+	public:
+		PresenceMessageContent(string from, string to, string onlineStatus, string onlineStatusDesc);
+		PresenceMessageContent(const string &buildFrom);
+		virtual std::string getMemObjectType(){return "PresenceMessageContent";}
+		virtual std::string getString();
+		virtual std::string getContentType(){return "application/xpidf+xml";}
+
+
+	private:
+		string fromUri;
+		string toUri;
+		string document;
+		string onlineStatus;
+		string onlineStatusDesc;
+		string presentity;
+};
+
+
+#endif
