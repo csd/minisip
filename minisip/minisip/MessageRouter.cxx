765f5c6fc7ab59917d293baf44942bf32c5c4b0d
jobi <jobi@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-06-08 07:50:35 +0000

    guicb_doInvite is still used though
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1321 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- minisip/minisip/MessageRouter.cxx
+++ minisip/minisip/MessageRouter.cxx
@@ -164,12 +164,10 @@ void MessageRouter::guicb_handleMediaCommand(CommandString &cmd){
 	mediaHandler->handleCommand(cmd);
 }
 
-/*
 string MessageRouter::guicb_doInvite(string user){
 //	cerr << "ERROR: INVITE USER UNIMPLEMENTED"<< endl;
 	return sip->invite(user);
 }
-*/
 
 void MessageRouter::guicb_handleCommand(CommandString &cmd){
 	//return sip_machine->enqueueCommand(cmd);
