3e96d4f0496d9432537e7e9b6a7db3b90d2d8da3
javier <javier@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-07-16 02:47:40 +0000

    Cesc noted that the script was not handling inter-library dependencies correctly.  Fixed it.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1450 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- buildall.sh
+++ buildall.sh
@@ -18,16 +18,15 @@ do
 	echo Building $subdir...
 	cd ${subdir}
 	./bootstrap
-	./configure --enable-debug
+	LDFLAGS="$LDFLAGS -L$PWD/.libs"
+	CXXFLAGS="$CXXFLAGS -I$PWD/include"
+	LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" ./configure --enable-debug
 	make
-	MINISIP_LDFLAGS="$MINISIP_LDFLAGS -L$PWD/.libs"
-	MINISIP_CXXFLAGS="$MINISIP_CXXFLAGS -I$PWD/include"
-	echo $MINISIP_LDFLAGS $MINISIP_CXXFLAGS
 	cd ..
 done
 
 cd minisip
 ./bootstrap
-LDFLAGS=$MINISIP_LDFLAGS CXXFLAGS=$MINISIP_CXXFLAGS \
+LDFLAGS=$LDFLAGS CXXFLAGS=$CXXFLAGS \
 	./configure --enable-debug --enable-color-terminal
 make
