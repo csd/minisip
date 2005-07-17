4563f15bb8868ab40cef3cd861f8f5487f34d479
cesc <cesc@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-07-17 16:03:03 +0000

    Ok, after trying gazillions of combinations, this seems to be the only that works (for me). Javier, sorry for using exports, but it reallys seems that otherwise the configure for minisip app wouldn't work. Feel free to test it and modify it at will ... maybe it does not work for you.
    I added the buildonly.sh script, which speeds up small recompilations ... this needs no exports ;)
    
    
    
    
    
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1471 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- buildall.sh
+++ buildall.sh
@@ -6,6 +6,15 @@
 # will build minisip and with all the associated libraries.  The build will be
 # done "in place", that is, no files will be installed on your system.
 #
+# Instructions: 
+#  Use buildall.sh script the first time you get the sources of minisip. It will
+# create all files needed and compile all the sources. 
+# Then, if you modify a file in the sources and do not want to recompile all 
+# MiniSIP again, use the "buildonly.sh" script
+
+# Note that you con modify the configuration of minisip by editing the 
+# configure_params variable in this script. Some useful options are commented
+# out by default, but feel free to use them.
 
 SUBDIRS="${SUBDIRS} libmutil"
 SUBDIRS="${SUBDIRS} libmnetutil"
@@ -15,18 +24,77 @@ SUBDIRS="${SUBDIRS} libminisip"
 
 for subdir in ${SUBDIRS}
 do
-	echo Building $subdir...
+	echo "+++++++++++++++++++++++++++++++++++++"
+	echo "Building ${subdir} ... "
+	echo "+++++++++++++++++++++++++++++++++++++"
+	echo
+	echo "LDFLAGS = $LOC_LDFLAGS" 
+	echo "CXXFLAGS = $LOC_CXXFLAGS"
+	echo
+	echo "+++++++++++++++++++++++++++++++++++++"
+		
 	cd ${subdir}
 	./bootstrap
-	LDFLAGS="$LDFLAGS -L$PWD/.libs"
-	CXXFLAGS="$CXXFLAGS -I$PWD/include"
-	LDFLAGS="$LDFLAGS" CXXFLAGS="$CXXFLAGS" ./configure --enable-debug
+
+	#Possible configure commands are shown in: ./configure --help
+	#  Usefull: 
+	#     enable-debug (lot more output, helpfull with problems)
+	#     enble-shared (compile only static libs ... speeds up compilation)
+
+	configure_params=""
+#	configure_params="$configure_params --enable-debug "
+#	configure_params="$configure_params --disable-shared"
+	
+	if [ ${subdir} = "libmutil" ]; then 
+		echo libmutil can use special params
+		#here you can add the --enable-memdebug, only for libmutil
+#		configure_params="$configure_params --enable-memdebug"
+	fi 
+	echo "=========================================================="
+	echo "configure_params (${subdir})= $configure_params"
+	echo "=========================================================="
+	
+	
+#	LDFLAGS=$LOC_LDFLAGS CXXFLAGS=$LOC_CXXFLAGS ./configure $configure_params
+	
+	./configure $configure_params
+
 	make
+	
+	LOC_LDFLAGS="$LOC_LDFLAGS -L$PWD/.libs"
+	LOC_CXXFLAGS="$LOC_CXXFLAGS -I$PWD/include"
+	export LDFLAGS=$LOC_LDFLAGS
+	export CXXFLAGS=$LOC_CXXFLAGS 
+	
 	cd ..
 done
 
+echo "+++++++++++++++++++++++++++++++++++++"
+echo "Building MiniSIP application ... "
+echo "+++++++++++++++++++++++++++++++++++++"
+
 cd minisip
 ./bootstrap
-LDFLAGS=$LDFLAGS CXXFLAGS=$CXXFLAGS \
-	./configure --enable-debug --enable-color-terminal
+
+configure_params="--enable-color-terminal"
+#configure_params="$configure_params --enable-alsa"
+#configure_params="$configure_params --enable-debug"
+#configure_params="$configure_params --disable-shared"
+
+export LDFLAGS=$LOC_LDFLAGS
+export CXXFLAGS=$LOC_CXXFLAGS 
+
+echo
+echo
+echo "LDFLAGS = $LDFLAGS" 
+echo "CXXFLAGS = $CXXFLAGS"
+echo
+echo "=========================================================="
+echo "configure_params (minisip)= $configure_params"
+echo "=========================================================="
+
+./configure $configure_params
+
 make
+
+
---
+++ buildonly.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+#
+# Simple script to build minisip.
+#
+# This script needs to be executed at the root of the repository trunk.  It
+# will build minisip and with all the associated libraries.  The build will be
+# done "in place", that is, no files will be installed on your system.
+#
+# This script only runs a make into every folder. It needs buildall.sh to be
+#first. Note that libraries, at minimum, will be relinked, to ensure that all
+#changes are caught.
+
+SUBDIRS="${SUBDIRS} libmutil"
+SUBDIRS="${SUBDIRS} libmnetutil"
+SUBDIRS="${SUBDIRS} libmikey"
+SUBDIRS="${SUBDIRS} libmsip"
+SUBDIRS="${SUBDIRS} libminisip"
+
+for subdir in ${SUBDIRS}
+do
+	echo "+++++++++++++++++++++++++++++++++++++"
+	echo "Building ${subdir} ... "
+	echo "+++++++++++++++++++++++++++++++++++++"
+
+	cd ${subdir}
+		
+	#force to relink ... 	
+	rm -f ${subdir}.la
+	
+	LDFLAGS="$LDFLAGS -L$PWD/.libs"
+	CXXFLAGS="$CXXFLAGS -I$PWD/include"
+
+	make
+	
+	echo
+	echo
+	echo "LDFLAGS = $LDFLAGS" 
+	echo "CXXFLAGS = $CXXFLAGS"
+	echo
+	
+	cd ..
+done
+
+echo "+++++++++++++++++++++++++++++++++++++"
+echo "Building MiniSIP application ... "
+echo "+++++++++++++++++++++++++++++++++++++"
+
+cd minisip
+
+#force to relink
+rm -f ./soundcard/libsoundcard.a
+rm -f ./conf/libconf.a
+rm -f ./sdp/libsdp.a
+rm -f ./aec/libaec.a
+rm -f ./stun/libstun.a
+rm -f ./rtp/librtp.a
+rm -f ./minisip/ipprovider/libipprovider.a
+rm -f ./minisip/gui/gtkgui/libminisip_gtkgui.a
+rm -f ./minisip/gui/libminisip_gui.a
+rm -f ./minisip/contactdb/libcontactdb.a
+rm -f ./minisip/libminisip.a
+rm -f ./mediahandler/libmediahandler.a
+rm -f ./sip/libsip.a
+rm -f ./codecs/g711/libcodec_g711.a
+rm -f ./codecs/ilbc/libcodec_ilbc.a
+rm -f ./codecs/libcodecs.a
+rm -f ./spaudio/libspaudio.a
+rm -f minisip/minisip
+
+LDFLAGS="$LDFLAGS -L$PWD/.libs"
+CXXFLAGS="$CXXFLAGS -I$PWD/include"
+echo
+echo
+echo "LDFLAGS = $LDFLAGS" 
+echo "CXXFLAGS = $CXXFLAGS"
+echo
+
+make
