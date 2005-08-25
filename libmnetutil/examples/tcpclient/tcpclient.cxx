62375ac245dde8396ba60d5774121c2a36a1d7ad
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-08-25 12:22:17 +0000

    * Added tcp client example  * Makefile of tcpserver included search path that did not make sence to    all but me (incorrect -L and -I).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1563 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmnetutil/examples/tcpclient/tcpclient.cxx
@@ -0,0 +1,45 @@
+
+
+#include<libmnetutil/TCPSocket.h> 
+
+#include<libmnetutil/NetworkException.h> //ConnectFailed && HostNotFound
+
+#include<libmutil/trim.h>  //removes whitespace from start and end of strings
+
+int main(int argc, char **argv){
+
+	if (argc!=4){
+		cerr << "Usage: tcpclient <serveraddress> <serverport> <texttosend>\nExample:  ./tcpclient localhost 3333 \"Hello world\"."<<endl;
+		return 1;
+	}
+	
+
+	TCPSocket *sock;
+	try{
+		//Create a TCP connection to the server (address and port given
+		//as arguments to the application).
+		sock = new TCPSocket(argv[1], atoi(argv[2]));
+	}catch(ConnectFailed *){
+		cerr << "Sorry, I could not connect to port "<< argv[2] << " on the server." << endl;
+		return -3;
+	
+	}catch(HostNotFound *){
+		cerr << "Sorry, the server <"<< argv[1]<<"> could not be found"<< endl;
+		return -3;
+	}
+	
+	//Send some data to the client
+	sock->write(argv[3]);
+
+	//Read some data from the server
+	char buf[10000];
+	int nread = sock->read(buf, 10000);
+	buf[nread]=0;
+
+	//Print what we read to the screen
+	cerr << "Received from server: "<< buf<<endl;
+	
+	sock->close();
+	
+	return 0;
+}
