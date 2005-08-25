73bbdf3bfb06ebb2d79e4632fda4c05a3756ea68
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-08-25 14:00:02 +0000

    * Cleaned up headers and replacing type definitions with  	 "libmutil/mtypes.h"
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1567 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmutil/include/libmutil/mtypes.h
+++ libmutil/include/libmutil/mtypes.h
@@ -4,8 +4,12 @@
 // Add cross-platform, cross library/application types in this file.
 
 #if defined _MSC_VER
+
+typedef unsigned short uint16_t;
+
+typedef __int32  int32_t;
 typedef unsigned int uint32_t;
-typedef int int32_t;
+
 typedef unsigned long long uint64_t
 
 #else
