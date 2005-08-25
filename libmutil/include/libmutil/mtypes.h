bc63fe73351c38719c8e475c7c7b309a429c20d8
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-08-25 09:27:24 +0000

    * Added mtypes.h that contains cross-library type definitions (for example    integer defines).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1558 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmutil/include/libmutil/mtypes.h
@@ -0,0 +1,15 @@
+#ifndef _MTYPES_H
+#define _MTYPES_H
+
+// Add cross-platform, cross library/application types in this file.
+
+#if defined _MSC_VER
+typedef unsigned int uint32_t;
+typedef int int32_t;
+#else
+#include<stdint.h>
+#endif 
+
+typedef uint8_t byte_t;
+
+#endif
