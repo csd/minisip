660569d79e5c6d83ada3093f36590347b6573686
cesc <cesc@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-09-18 21:38:55 +0000

    Add a run script for minisip. Just run this script, there is no need to copy any file, anywhere (i think).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1617 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ run.minisip.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+#
+# Simple script to run minisip
+#
+
+SUBDIRS="${SUBDIRS} libmutil"
+SUBDIRS="${SUBDIRS} libmnetutil"
+SUBDIRS="${SUBDIRS} libmikey"
+SUBDIRS="${SUBDIRS} libmsip"
+SUBDIRS="${SUBDIRS} libminisip"
+
+MY_LD_LIB_PATH="$LD_LIBRARY_PATH"
+
+for subdir in ${SUBDIRS}
+do
+	cd ${subdir}
+	MY_LD_LIB_PATH="$MY_LD_LIB_PATH$PWD/.libs:"
+	cd ..
+done
+
+cd minisip
+MY_LD_LIB_PATH="$MY_LD_LIB_PATH$PWD/minisip/.libs"
+
+cd ..
+echo "export LD_LIBRARY_PATH=$MY_LD_LIB_PATH"
+
+LD_LIBRARY_PATH="$MY_LD_LIB_PATH" minisip/minisip/minisip
+
+
