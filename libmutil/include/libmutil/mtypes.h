8078f837a3f478e9a4920d01e9e6341fd4281feb
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-10-12 08:56:55 +0000

    * libmutil/mtypes.h: Added some more integer type definitions    for W32 (problem reported by Andreas Angstrom).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1669 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmutil/include/libmutil/mtypes.h
+++ libmutil/include/libmutil/mtypes.h
@@ -5,12 +5,18 @@
 
 #if defined _MSC_VER
 
+typedef unsigned char uint8_t;
+typedef char int8_t;
+
 typedef unsigned short uint16_t;
+typedef short int16_t;
 
 typedef __int32  int32_t;
 typedef unsigned int uint32_t;
 
 typedef unsigned long long uint64_t;
+typedef long long int64_t;
+
 
 #else
 #include<stdint.h>
