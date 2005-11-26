ce9205899ea5fcea618d38c642753184fbfd9666
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-11-26 16:29:56 +0000

    * libmutil: W32 port: Our massert add no value on W32 since     we don't have any stack trace functionality there. It is     better to run the ordinary "assert" (it has extended     functionality if the user is running debug libraries).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1817 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmutil/include/libmutil/massert.h
+++ libmutil/include/libmutil/massert.h
@@ -1,7 +1,19 @@
 #ifndef _MASSERT_H
 #define _MASSERT_H
 
-void massertFailed(char *expr, char *file, char *baseFile, int line);
+#ifdef _MSC_VER
+#ifdef LIBMUTIL_EXPORTS
+#define LIBMUTIL_API __declspec(dllexport)
+#else
+#define LIBMUTIL_API __declspec(dllimport)
+#endif
+#else
+#define LIBMUTIL_API
+#endif
+
+
+
+LIBMUTIL_API void massertFailed(char *expr, char *file, char *baseFile, int line);
 
 #ifdef NDEBUG
 #define massert(exp)
@@ -13,7 +25,17 @@ void massertFailed(char *expr, char *file, char *baseFile, int line);
  * Note that the stack trace will contain calls to
  * "massertFailed" and "getStackTraceString".
  */
+
+#ifdef _MSC_VER
+#include<assert.h>
+#define massert(exp) assert(exp)
+#else
+#ifdef __BASE_FILE__
 #define massert(exp) if (exp) ; else massertFailed(#exp, __FILE__, __BASE_FILE__, __LINE__)
+#else
+#define massert(exp) if (exp) ; else massertFailed(#exp, __FILE__, __FILE__, __LINE__)
+#endif
+#endif
 #endif
 
 #endif
