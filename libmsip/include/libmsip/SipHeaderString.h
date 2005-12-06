6089387fd40deba1300e5ab9c6d6c283cb3dd309
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-12-06 20:27:59 +0000

    * Several headers in libmsip has very little complexity and only    holds a string. This is a class that can be used as a base class    for all such header value classes.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1859 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmsip/include/libmsip/SipHeaderString.h
@@ -0,0 +1,82 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+*/
+
+
+/* Name
+ * 	SipHeaderString.h
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 	Represents a header value that internally is stored
+ * 	as a string, such as the Subject or Content-Type header 
+ * 	values.
+*/
+
+#ifndef SIPHEADERSTRING_H
+#define SIPHEADERSTRING_H
+
+#ifdef _MSC_VER
+#ifdef LIBMSIP_EXPORTS
+#define LIBMSIP_API __declspec(dllexport)
+#else
+#define LIBMSIP_API __declspec(dllimport)
+#endif
+#else
+#define LIBMSIP_API
+#endif
+
+#include<libmsip/SipHeader.h>
+
+/**
+ * @author Erik Eliasson
+*/
+
+extern SipHeaderFactoryFuncPtr sipHeaderSubjectFactory;
+
+class LIBMSIP_API SipHeaderValueString: public SipHeaderValue{
+	public:
+
+		/**
+		 * @param type	Header type identifier as defined
+		 * 		in SipHeader.h. This is needed since
+		 * 		this does not represent a SIP header
+		 * 		value by it self, and is expected to
+		 * 		be used as base class for "Subject",
+		 * 		"Content-type" etc.
+		 * @param build_from	String that is the header value.
+		 */
+		SipHeaderValueString(int type, const string& typeStr, const string& build_from);
+
+		virtual ~SipHeaderValueString();
+		
+		/**
+		 * returns string representation of the header. This
+		 * 	is the string passed to the constructor
+		 * 	with any white space in the ends removed.
+		 */
+		string getString(); 
+
+	protected:
+		string str;
+};
+
+#endif
