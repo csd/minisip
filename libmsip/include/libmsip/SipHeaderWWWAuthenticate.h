db8432ed710e34507e8f9e05e80e36f3a4ba9876
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2005-12-07 02:33:54 +0000

    * Added support for WWW-Authenticate header. We supported answering to    proxies that requested authorization, but it was a hack where we    extracted nonce and realm during message parsing.
       More than one proxy that requires authentication during one call
       has never been tested and will probably not work (We probably only
       send one authentication header).
    
    
    
    
    git-svn-id: svn://svn.minisip.org/minisip/trunk@1870 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmsip/include/libmsip/SipHeaderWWWAuthenticate.h
@@ -0,0 +1,64 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+
+/* Name
+ * 	SipHeaderProxyAuthenticate.h
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 
+*/
+
+#ifndef SIPHEADERWWWAUTHENTICATE_H
+#define SIPHEADERWWWAUTHENTICATE_H
+
+#include<libmsip/libmsip_config.h>
+
+
+#include<libmsip/SipHeaderProxyAuthenticate.h>
+#include<libmsip/SipURI.h>
+
+/**
+ * @author Erik Eliasson
+*/
+
+extern SipHeaderFactoryFuncPtr sipHeaderWWWAuthenticateFactory;
+
+/**
+ *
+ * A WWW-Authenticate header value is handled the same way as
+ * a Proxy-Authenticate.
+ */
+class LIBMSIP_API SipHeaderValueWWWAuthenticate: public SipHeaderValueProxyAuthenticate{
+	public:
+		SipHeaderValueWWWAuthenticate(const string &build_from);
+
+		virtual ~SipHeaderValueWWWAuthenticate();
+
+                virtual std::string getMemObjectType(){return "SipHeaderWWWAuthenticate";}
+		
+	private:
+};
+
+#endif
---
+++ libmsip/source/headers/SipHeaderWWWAuthenticate.cxx
@@ -0,0 +1,56 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+
+/* Name
+ * 	SipHeaderValueWWWAuthenticate.cxx
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 
+*/
+
+#include<config.h>
+
+#include<libmutil/trim.h>
+#include<libmsip/SipHeaderWWWAuthenticate.h>
+
+MRef<SipHeaderValue *> wwwAuthFactory(const string &build_from){
+	                return new SipHeaderValueWWWAuthenticate(build_from);
+}
+
+SipHeaderFactoryFuncPtr sipHeaderWWWAuthenticateFactory=wwwAuthFactory;
+
+
+const string sipHeaderValueWWWAuthenticationTypeStr = "WWW-Authenticate";
+		
+SipHeaderValueWWWAuthenticate::SipHeaderValueWWWAuthenticate(const string &build_from)
+		: SipHeaderValueProxyAuthenticate(SIP_HEADER_TYPE_WWWAUTHENTICATE,sipHeaderValueWWWAuthenticationTypeStr, build_from)
+{
+	cerr << "SipHeaderValueWWWAuthenticate constructor running with <"<<build_from<<">"<<endl;
+}
+
+SipHeaderValueWWWAuthenticate::~SipHeaderValueWWWAuthenticate(){
+
+}
+		
