97d771ae067032637091479d179148aa539855a3
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-03-24 14:05:05 +0000

    * Moved MessageRouter implementation to libmutil. This is a more general    message passing class where "subsystems" can be registred during    run-time.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2046 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmutil/include/libmutil/MessageRouter.h
@@ -0,0 +1,144 @@
+/*
+  Copyright (C) 2006 Erik Eliasson
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef MESSAGE_ROUTER_H
+#define MESSAGE_ROUTER_H
+
+#include <libmutil/libmutil_config.h>
+
+#include<libmutil/mtypes.h>
+#include<libmutil/MemObject.h>
+#include<libmutil/CommandString.h>
+#include<libmutil/Exception.h>
+#include<string>
+
+/**
+ * Thrown when handleCommand/handleCommandResp in SipMessageRouter
+ * is called with a subsystem that is not registred (using the
+ * addSubsystem in the MessageRouter class).
+ */
+class SubsystemNotFoundException : public Exception{
+	public:
+		/**
+		 * @param what	Name of the subsystem not found. The name
+		 * 		can be retrieved using the what() method.
+		 */
+		SubsystemNotFoundException(const char *what);
+};
+
+/**
+ * Interface for a subsystem that receives CommandString messages.
+ */
+class CommandReceiver : public virtual MObject{
+	public:
+		/**
+		 * Note that the second argument is sent as reference (&) and
+		 * not as the more appropriate const reference (const&).
+		 * This is only because of how CommandString is implemented
+		 * (internally it uses a map<> and retrieving values from
+		 * it is not declared as const and therefore inspecting
+		 * constant CommandStrings is not possible) and classes 
+		 * implementing CommandReceiver interface SHOULD NOT modify 
+		 * the command parameter.
+		 * 
+		 * @param subsystem	Name of the subsystem that the message 
+		 * 			is being sent to. This parameter can 
+		 * 			be inspected if a class implements 
+		 * 			several subsystems.
+		 * @param command	Contains destination id, operation
+		 * 			with parameters.
+		 */
+		virtual void handleCommand(std::string subsystem, const CommandString& command)=0;
+
+		/**
+		 * Same as handleCommand except that it has a return
+		 * value.
+		 */
+		virtual CommandString handleCommandResp(std::string subsystem, const CommandString&)=0;
+};
+
+class MessageRouterInternal;
+
+/**
+ * Implements message passing to subsystems. Subsystems are registred with
+ * a name and messages are passed with subsystem name as destination.
+ * Subsystems must implement the CommandReceiver interface and the 
+ * handleCommand and handleCommandResp is used to pass messages.
+ *
+ * If a command is sent to a unknown subsystem the MessageRouter will
+ * throw a SubsystemNotFoundException exception.
+ *
+ */
+class MessageRouter : public CommandReceiver{
+	public:
+		MessageRouter();
+		~MessageRouter();
+
+		/**
+		 * Returns true if a subsystem with the name passed as
+		 * parameter has been registred with this MessageRouter
+		 * instance.
+		 * @param name	Name of a subsystem.
+		 * @return	true if a subsystem with that name has been
+		 * 		registred and false if not.
+		 *
+		 */
+		bool hasSubsystem(std::string name);
+
+		/**
+		 * Registers a subsystem.
+		 *
+		 * @param name	Name of the subsystem
+		 * @param ss	Subsystem that will receive messagages.
+		 * @return True if the subsystem was successfully added.
+		 * 	   False if the subsystem already existed and could
+		 * 	   not be added.
+		 */
+		bool addSubsystem(std::string name, MRef<CommandReceiver*> ss);
+
+		/**
+		 * @param subsystem	Destination subsystem name.
+		 * @param cmd		Command that will be passed to the
+		 * 			handleCommand method of the
+		 * 			subsystem object registred using
+		 * 			addSubsystem.
+		 */
+		void handleCommand(std::string subsystem, const CommandString& cmd);
+
+		/**
+		 *
+		 * @param subsystem	Destination subsystem name.
+		 * @param cmd		Command that will be passed to the
+		 * 			handleCommand method of the
+		 * 			subsystem object registred using
+		 * 			addSubsystem.
+		 * @return Return value of the handleCommandResp method of
+		 * 	   the subsystem.
+		 */
+		CommandString handleCommandResp(std::string subsystem, const CommandString& cmd);
+
+		/**
+		 * Removes all subsystems.
+		 */
+		void clear();
+		
+	private:
+		MessageRouterInternal *internal;
+};
+
+#endif
---
+++ libmutil/source/MessageRouter.cxx
@@ -0,0 +1,107 @@
+#include<config.h>
+
+#include<libmutil/MessageRouter.h>
+#include<string>
+
+using namespace std;
+
+/* 
+ * The MessageRouter internal state is a collection of 
+ * (subsystem name, CommandReceiver object reference)
+ * tuples. Such a tuple is called a "Route".
+ *
+ * Todo: Plugin slots:
+ *  - Before a message is routed
+ *  - After the message has been delivered
+ *  This can be used by a logging/monitoring plugin.
+ *  
+ */
+
+
+SubsystemNotFoundException::SubsystemNotFoundException(const char *what):Exception(what){
+
+}
+
+/**
+ * Tuple containing subsystem name and reference to the subsystem interface object.
+ */
+class Route{
+	public:
+		string ssname;
+		MRef<CommandReceiver*> dest;
+};
+
+class MessageRouterInternal{
+	public:
+		list<Route> subsystems;
+};
+
+
+MessageRouter::MessageRouter(){
+	internal = new MessageRouterInternal;
+}
+
+MessageRouter::~MessageRouter(){
+	delete internal;
+}
+
+bool MessageRouter::hasSubsystem(string name){
+	list<Route>::iterator i;
+	for (i=internal->subsystems.begin(); i!=internal->subsystems.end(); i++){
+		if ((*i).ssname==name){
+			return true;
+		}
+	}
+	return false;
+}
+
+bool MessageRouter::addSubsystem(string name, MRef<CommandReceiver*> rcvr){
+	if (hasSubsystem(name)){
+		return false;
+	}else{
+		Route r;
+		r.ssname = name;
+		r.dest = rcvr;
+		internal->subsystems.push_back(r);
+		return true;
+	}
+}
+
+void MessageRouter::handleCommand(string subsystem, const CommandString &cmd){
+#ifdef DEBUG_OUTPUT
+	cerr << "MessageRouter:  To:"<<subsystem<<" Command:"<<cmd.getString()<<endl;
+#endif
+	list<Route>::iterator i;
+	for (i=internal->subsystems.begin(); i!=internal->subsystems.end(); i++){
+		if ((*i).ssname==subsystem){
+			//cerr << "Comparing <"<<subsystem <<"> with <"<<(*i).ssname<<">"<<endl;
+			(*i).dest->handleCommand(subsystem, cmd);
+			return;
+		}
+	}
+	throw new SubsystemNotFoundException(subsystem.c_str());
+}
+
+CommandString MessageRouter::handleCommandResp(string subsystem, const CommandString &cmd){
+#ifdef DEBUG_OUTPUT
+	cerr << "MessageRouter:  To(request):"<<subsystem<<" Command:"<<cmd.getString()<<endl;
+#endif
+	list<Route>::iterator i;
+	for (i=internal->subsystems.begin(); i!=internal->subsystems.end(); i++){
+		if ((*i).ssname==subsystem){
+			CommandString ret =(*i).dest->handleCommandResp(subsystem, cmd);
+#ifdef DEBUG_OUTPUT
+			cerr << "MessageRouter:  Response from:"<<subsystem<<" Command:"<<ret.getString()<<endl;
+#endif
+			return ret;
+		}
+	}
+	throw new SubsystemNotFoundException(subsystem.c_str());
+	CommandString dummy("","");
+	return dummy; //not reached - avoids compiler warning
+}
+
+void MessageRouter::clear(){
+	internal->subsystems.clear();
+}
+
