649592f2f52da7fd94002e7eb19750b928aa1641
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-04-08 12:54:39 +0000

    * The debug functionality of the text UI used some methods in libmsip to    inspect the state of the SIP stack. Some methods have moved, and this    fixes the compilation.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2153 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- minisip/minisip/gui/textui/MinisipTextUI.cxx
+++ minisip/minisip/gui/textui/MinisipTextUI.cxx
@@ -31,8 +31,8 @@
 #include<libmutil/trim.h>
 #include<libmutil/termmanip.h>
 
-#include<libmsip/SipDialogContainer.h>
 #include<libmsip/SipCommandString.h>
+#include<libmsip/SipMessageDispatcher.h>
 #include<libminisip/sip/DefaultDialogHandler.h>
 
 #include<libminisip/conference/ConfMessageRouter.h>
@@ -488,7 +488,7 @@ bool MinisipTextUI::configDialog( MRef<SipSoftPhoneConfiguration *> /*conf*/ ){
 
 void MinisipTextUI::showCalls(){
 	//list<MRef<SipDialog*> > *calls = config->sip->getDialogContainer()->getDispatcher()->getDialogs();
-	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDialogContainer()->getDispatcher()->getDialogs();
+	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDispatcher()->getDialogs();
 	displayMessage(string("Calls:"));
 	if (calls.size()==0)
 		displayMessage("    (no calls)");
@@ -505,7 +505,7 @@ void MinisipTextUI::showCalls(){
 }
 
 void MinisipTextUI::showTransactions(string command){
-	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDialogContainer()->getDispatcher()->getDialogs();
+	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDispatcher()->getDialogs();
 
 	string tno = trim(command).substr(17);
 	int itno=0;
@@ -640,12 +640,12 @@ void MinisipTextUI::showDialogInfo(MRef<SipDialog*> d, bool usesStateMachine, st
 }
 
 void MinisipTextUI::showStat(){
-	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDialogContainer()->getDispatcher()->getDialogs();
+	list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDispatcher()->getDialogs();
 
 	list <TPRequest<string, MRef<StateMachine<SipSMCommand,string>*> > > torequests = config->sip->getSipStack()->getTimeoutProvider()->getTimeoutRequests();
 
 //	displayMessage(" Default dialog handler:", bold);
-	showDialogInfo(config->sip->getSipStack()->getDialogContainer()->getDefaultHandler(), false, "  Default dialog handler: ");
+//	showDialogInfo(config->sip->getSipStack()->getDefaultHandler(), false, "  Default dialog handler: ");
 
 	displayMessage(" Calls:", bold);
 	if (calls.size()==0)
@@ -880,7 +880,7 @@ void MinisipTextUI::guiExecute(string cmd){
 	if (command == "show all"){
 		showCalls();
 
-		list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDialogContainer()->getDispatcher()->getDialogs();
+		list<MRef<SipDialog*> > calls = config->sip->getSipStack()->getDispatcher()->getDialogs();
 		for (uint32_t i=0; i<calls.size(); i++){
 			showTransactions("show transactions "+itoa(i));
 		}
