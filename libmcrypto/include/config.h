f6cc614b9eced6929897b4e5317477d9407451c5
zwelch <zwelch@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-04-27 08:49:56 +0000

    Bring libmcrypto internal config.h up-to-date with libmutil; add compile test.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2328 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmcrypto/include/config.h
+++ libmcrypto/include/config.h
@@ -23,48 +23,84 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#include<stdint.h>
-
 /* Compilation time configuration */
-#include"compilation_config.h"
-
-/* STL replacement */
-
-#ifdef USE_STL
-using namespace std;
-#include<string>
-
+#ifndef _WIN32_WCE
+#	include"compilation_config.h"
 #else
-#include<String.h>
-namespace minisip{
-	        typedef string String;
-}
-using namespace minisip;
-
+#	include"compilation_config_w32_wce.h"
 #endif
 
-typedef uint8_t byte_t;
+#ifndef LIBMUTIL_EXPORTS
+# ifdef DLL_EXPORT
+#  define LIBMUTIL_EXPORTS
+#  define OPENSSL_OPT_WINDLL	// import Windows dll
+# endif	 // DLL_EXPORT
+#endif	// LIBMUTIL_EXPORTS
 
-/* big/little endian conversion */
+#include<libmutil/mtypes.h>
+
+#ifdef _MSC_VER
+	#define WIN32
+	#pragma warning (disable: 4251)
+
+	#ifndef LIBMUTIL_EXPORTS
+		#error Visual Studio is not set up correctly to compile libmutil to a .dll (LIBMUTIL_EXPORTS not defined).
+	#endif
+#endif
+
+//Temporary ... STLPort does not allow addition of errno.h ... but WCEcompat does ... 
+//So we don't have to repeat this everytime, include errno.h for all files ... 
+//	anyway, it is just an int :)
+//Anyway, while compiling for EVC, it will still trigger some warnings ... ignore them, errno exhists for sure.
+#ifdef _WIN32_WCE
+#	ifndef _STLP_NATIVE_ERRNO_H_INCLUDED
+#		include<wcecompat/errno.h>
+#		define _STLP_NATIVE_ERRNO_H_INCLUDED
+#	endif
+#else
+#	include <errno.h>
+#endif
+/*big/little endian conversion*/
 
 static inline uint16_t U16_AT( void const * _p )
 {
-    uint8_t * p = (uint8_t *)_p;
+    const uint8_t * p = (const uint8_t *)_p;
     return ( ((uint16_t)p[0] << 8) | p[1] );
 }
 static inline uint32_t U32_AT( void const * _p )
 {
-    uint8_t * p = (uint8_t *)_p;
+    const uint8_t * p = (const uint8_t *)_p;
     return ( ((uint32_t)p[0] << 24) | ((uint32_t)p[1] << 16)
               | ((uint32_t)p[2] << 8) | p[3] );
 }
 static inline uint64_t U64_AT( void const * _p )
 {
-    uint8_t * p = (uint8_t *)_p;
+    const uint8_t * p = (const uint8_t *)_p;
     return ( ((uint64_t)p[0] << 56) | ((uint64_t)p[1] << 48)
               | ((uint64_t)p[2] << 40) | ((uint64_t)p[3] << 32)
               | ((uint64_t)p[4] << 24) | ((uint64_t)p[5] << 16)
               | ((uint64_t)p[6] << 8) | p[7] );
 }
+#if defined WORDS_BIGENDIAN
+#   define hton16(i)   ( i )
+#   define hton32(i)   ( i )
+#   define hton64(i)   ( i )
+#   define ntoh16(i)   ( i ) 
+#   define ntoh32(i)   ( i )
+#   define ntoh64(i)   ( i )
+#else
+#   define hton16(i)   U16_AT(&i)
+#   define hton32(i)   U32_AT(&i)
+#   define hton64(i)   U64_AT(&i)
+#   define ntoh16(i)   U16_AT(&i)
+#   define ntoh32(i)   U32_AT(&i)
+#   define ntoh64(i)   U64_AT(&i)
+#endif
+
+
+#ifdef DEBUG_OUTPUT
+#define MSM_DEBUG
+#define MSM_DEBUG_COMMAND
+#endif
 
 #endif
---
+++ libmcrypto/tests/000_compile.cxx
@@ -0,0 +1,7 @@
+#include <config.h>
+#include <libmcrypto/aes.h>
+#include <libmcrypto/base64.h>
+#include <libmcrypto/hmac.h>
+#include <libmcrypto/cert.h>
+
+int main(int argc, char *argv[]) { }
