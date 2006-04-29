b2dffe3be661b646bedff4974aada35a9f175d92
mikma <mikma@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-04-29 21:04:52 +0000

    Moving the iLBC codec to a separate package since it isn't released under the GNU LGPL license used by libminisip.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2396 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ milbc/source/ilbc/getCBvec.h
@@ -0,0 +1,29 @@
+
+//A.23 getCBvec.h 
+    
+   /****************************************************************** 
+    
+       iLBC Speech Coder ANSI-C Source Code 
+    
+       getCBvec.h                  
+    
+       Copyright (c) 2001, 
+       Global IP Sound AB. 
+       All rights reserved. 
+    
+   ******************************************************************/ 
+    
+   #ifndef __iLBC_GETCBVEC_H 
+   #define __iLBC_GETCBVEC_H 
+    
+   void getCBvec( 
+       float *cbvec,   /* (o) Constructed codebook vector */ 
+       float *mem,     /* (i) Codebook buffer */ 
+       int index,      /* (i) Codebook index */ 
+       int lMem,       /* (i) Length of codebook buffer */ 
+       int cbveclen/* (i) Codebook vector length */ 
+   ); 
+    
+   #endif 
+    
+    
