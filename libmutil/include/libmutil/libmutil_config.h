c7a7fafc3c37aca6dcb70b37aa5763240c531885
mikma <mikma@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-05-04 21:29:43 +0000

    Support for LIBMUTIL_DEPRECATED to declaring a function/variable deprecated, supported by GCC >= 3.1. --denna och nedanstående rader kommer att tas bort--
    M    libmutil_config.h
    
    
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2419 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmutil/include/libmutil/libmutil_config.h
+++ libmutil/include/libmutil/libmutil_config.h
@@ -40,4 +40,10 @@
 # define LIBMUTIL_API
 #endif
 
+#if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
+#define LIBMUTIL_DEPRECATED  __attribute__((__deprecated__))
+#else
+#define LIBMUTIL_DEPRECATED
+#endif /* __GNUC__ */
+
 #endif	// _MLIBMUTIL_CONFIG_H
