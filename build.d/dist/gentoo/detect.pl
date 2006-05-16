a46add6b81a1cfc5ee3b2e16ab9f6f2287e59015
zwelch <zwelch@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-05-16 02:38:53 +0000

    Fix problems with gentoo and debian build system configurations.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2516 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- build.d/dist/gentoo/detect.pl
+++ build.d/dist/gentoo/detect.pl
@@ -1,2 +1,2 @@
-# Gentoo autodetection support (yes, it can be this easy!)
-set_dist_detect(sub { -e '/etc/gentoo_release' });
+# Gentoo autodetection support
+set_dist_detect(sub { -e '/etc/gentoo-release' });
