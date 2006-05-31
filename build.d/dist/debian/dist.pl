8f71c2c7f562d5554d8edd64218e278c1140fd79
mikma <mikma@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2006-05-31 09:49:38 +0000

    Add initial support for building non-native Debian packages to build.d (i.e. Debian source packages containing an orig tarball and diff.gz)
    git-svn-id: svn://svn.minisip.org/minisip/trunk@2572 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- build.d/dist/debian/dist.pl
+++ build.d/dist/debian/dist.pl
@@ -1,29 +1,36 @@
 ###
-# dist.pl - extends support for build.pl for Gentoo/emerge
+# dist.pl - extends support for build.pl for Debian/dpkg
 
-sub gentoo_distdir {
-	my $distdir = `portageq envvar DISTDIR` or die "can't get DISTDIR";
-	chomp $distdir;
-	print "+portageq envvar DISTDIR=$distdir\n" if $verbose;
-	return $distdir;
+our $debian_tarballsdir = "$topdir/build/tarballs";
+our $debian_buildareadir = "$topdir/build/build-area";
+our $debian_dir = "$topdir/debian";
+our $svn_url_base = 'minisip+svn://svn.minisip.org/minisip';
+
+#
+# debian_changes
+# return .changes file
+#
+sub debian_changes {
+    my $arch = `dpkg-architecture -qDEB_BUILD_ARCH`;
+
+    $arch =~ s/\s*//g;
+
+    my $versionline = `dpkg-parsechangelog -l${debian_dir}/$pkg/debian/changelog | grep ^Version`;
+
+    $versionline =~ /^Version: (.*)/;
+    my $version = $1;
+
+    my $changes = "$debian_buildareadir/${pkg}_${version}_${arch}.changes";
+    $changes = "$debian_buildareadir/${pkg}_${version}_source.changes" unless -e $changes;
+
+    return $changes;
 }
 
-sub gentoo_package {
-	warn "warning: The Gentoo plug-in does not support binary packaging.\n"
-		unless $quiet;
-	# XXX: Gentoo supports binary packages, but this plug-in doesn't;
-	# as such, this could be problematic and should be optionally enabled.
-	#return gentoo_merge('-b') if $gentoo_binary_packages;
-	# XXX: for now, we're okay if we have distfiles to merge
-	return distfiles();
-}
-
-sub gentoo_pkgfiles {
-	# For now, just return tarballs required for merge
-	return distfiles(); 
-}
-
-sub gentoo_merge {
+#
+# debian_package
+# build source and binary package
+#
+sub debian_package {
 	my @distfiles = distfiles();
 	unless (@distfiles) {
 		warn <<NEED_TARBALLS;
@@ -32,29 +39,97 @@ NEED_TARBALLS
 		return;
 	}
 
-	my $distdir = gentoo_distdir();
+	easy_mkdir($debian_tarballsdir);
+
 	for my $p ( @distfiles ) {
-		my $tgt = File::Spec->catdir($distdir, basename($p));
-		if ( -e $tgt ) {
-			print "+removing old $tgt\n" unless $quiet;
-			unlink($tgt) or die "can't unlink $tgt: $!"; 
-		}
-		print "+copying $p -> $tgt\n" unless $quiet;
-		unless (link($p, $tgt) or copy($p, $tgt)) {
-			die "copy failed: $p -> $tgt: $!";
-		}
+	    my $base = basename($p);
+
+	    if ( $base =~ /(.*)-(.*).tar.gz/ ) {
+		my $pkg = $1;
+		my $version = $2;
+		my $orig = "${pkg}_${version}.orig.tar.gz";
+		my $tgt = File::Spec->catdir($debian_tarballsdir, $orig);
+
+		act('debian: ln', qw( ln -sf ), $p, $tgt);
+	    } else {
+		warn "warning: Can't parse tarball version";
+		return;
+	    }
 	}
-	act('gentoo: merge', qw( sudo emerge ), $pkg, '--digest', @_);
+
+	easy_mkdir($debian_buildareadir);
+
+	my $svn_override = "tagsUrl=${svn_url_base}/tags/debian/${pkg},upsTagUrl=${svn_url_base}/tags/${pkg},buildArea=$debian_buildareadir,origDir=$debian_tarballsdir";
+
+	easy_chdir("$topdir/debian/$pkg");
+	act('debian: svn-buildpackage', qw( svn-buildpackage -us -uc -rfakeroot --svn-dont-clean --svn-ignore-new ), "--svn-override=${svn_override}" );
+
+	return debian_pkgfiles();
 }
 
-sub gentoo_purge {
-	act('gentoo: purge', qw( sudo emerge ), '-C', $pkg);
+
+#
+# debian_pkgfiles
+# all files listed in the .changes file
+#
+sub debian_pkgfiles {
+    my $changes = &debian_changes;
+
+    open(CHANGES, "< $changes") or die "can't read $changes";
+
+    my @files;
+    my $found = 0;
+
+    while(<CHANGES>) {
+	my $line = $_;
+
+	if ( $line =~ /^ / ) {
+	    if ( $found ) {
+		$line =~ s/^\s*//;
+		$line =~ s/\n$//;
+		my ($md4, $size, $section, $priority, $file) = split(/ /, $line);
+		push( @files, "$debian_buildareadir/$file" );
+	    }
+	} else {
+	    $found = $line =~ /^Files:/;
+	}
+    }
+    close(CHANGES);
+
+    return @files;
+}
+
+#
+# debian_pkgcontents
+# dump contents of all packages listed in .changes file
+#
+sub debian_pkgcontents {
+    my $changes = &debian_changes;
+
+    act('debian: debc', 'debc', $changes);
+}
+
+#
+# debian_install
+# install all packages listed in the .changes file
+sub debian_merge {
+    my $changes = &debian_changes;
+
+    act('debian: debi', qw( debi ), '--debs-dir', $debian_buildareadir, $changes );
+}
+
+# TODO
+# debian_purge
+# purge all packages listed in the .changes file
+sub debian_purge {
+    die "debian: purge unsupported";
 }
 
 set_dist_callbacks(
-		'pkgfiles' => \&gentoo_pkgfiles,
-		'package' => \&gentoo_package,
-		merge => \&gentoo_merge,
-		purge => \&gentoo_purge, 
+		'pkgfiles' => \&debian_pkgfiles,
+		'package' => \&debian_package,
+		pkgcontents => \&debian_pkgcontents,
+		merge => \&debian_install,
+		purge => \&debian_purge, 
 	);
 
