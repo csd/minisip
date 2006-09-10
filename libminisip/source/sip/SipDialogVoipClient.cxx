csd/minisip

commit 6f4cc71a14ccc136eba730219552a2d4362ae91e

* Compiler warning fixes
git-svn-id: svn://svn.minisip.org/minisip/trunk@2752 63dae60f-c5d3-0310-92a3-fcbf00261c72

libmsip/source/dialogs/SipDialog.cxx
@@ -66,7 +66,7 @@ SipDialog::SipDialog(MRef<SipStack*> stack, MRef<SipDialogConfig*> callconf):
 					
 	dialogState.isEarly=false;	//same as for "secure"?! -EE
 	dialogState.isEstablished = false;
-	dialogState.rseqNo = -1;
+	dialogState.rseqNo = 0;
 }
 
 SipDialog::~SipDialog(){
@@ -303,7 +303,8 @@ bool SipDialog::updateAuthentication( MRef<SipResponse*> resp,
 bool SipDialog::updateAuthentications( MRef<SipResponse*> resp ){
 	bool changed = false;
 
-	for( int i = 0;; i++ ){
+	int i;
+	for( i = 0;; i++ ){
 		MRef<SipHeaderValueWWWAuthenticate*> auth;
 		auth = resp->getHeaderValueWWWAuthenticate( i );
 
@@ -313,7 +314,7 @@ bool SipDialog::updateAuthentications( MRef<SipResponse*> resp ){
 		changed |= updateAuthentication(resp, *auth);
 	}
 
-	for( int i = 0;; i++ ){
+	for( i = 0;; i++ ){
 		MRef<SipHeaderValueProxyAuthenticate*> auth;
 		auth = resp->getHeaderValueProxyAuthenticate( i );
 

commit 3eb09e98e4e319230402f86e070e81cd0e650b16

* Fixed bug introduced in r2752 (PRACK support broken).
git-svn-id: svn://svn.minisip.org/minisip/trunk@2754 63dae60f-c5d3-0310-92a3-fcbf00261c72

libminisip/source/sip/SipDialogVoipClient.cxx
@@ -790,7 +790,7 @@ bool SipDialogVoipClient::handleRel1xx( MRef<SipResponse*> resp ){
 		
 	// First reliable provisional response
 	// Next in-order reliable provisional response
-	if( !(dialogState.rseqNo == (uint32_t)-1 ||
+	if( !(dialogState.rseqNo == -1 ||
 	      dialogState.rseqNo > rseqNo ) )
 		return false;
 

libmsip/include/libmsip/SipDialog.h
@@ -95,7 +95,7 @@ class LIBMSIP_API SipDialogState{
 		
 		int seqNo;
 		int remoteSeqNo;
-		uint32_t rseqNo;
+		int32_t rseqNo;
 		std::string localUri;	// not used yet
 
 		/**

libmsip/source/dialogs/SipDialog.cxx
@@ -66,7 +66,7 @@ SipDialog::SipDialog(MRef<SipStack*> stack, MRef<SipDialogConfig*> callconf):
 					
 	dialogState.isEarly=false;	//same as for "secure"?! -EE
 	dialogState.isEstablished = false;
-	dialogState.rseqNo = 0;
+	dialogState.rseqNo = -1;
 }
 
 SipDialog::~SipDialog(){