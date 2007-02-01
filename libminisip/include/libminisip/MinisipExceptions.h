8d16576c43c70b48abc80b6874f65c8bf71345b3
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2007-02-01 09:41:22 +0000

    * Added MinisipExceptions.{h,cxx}.
    git-svn-id: svn://svn.minisip.org/minisip/trunk@3171 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libminisip/include/libminisip/MinisipExceptions.h
@@ -0,0 +1,44 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+ */
+
+#ifndef MINISIP_EXCEPTION_H
+#define MINISIP_EXCEPTION_H
+
+#include<libminisip/libminisip_config.h>
+
+#include<string>
+#include<libmutil/Exception.h>
+
+class LIBMINISIP_API MinisipException : public Exception{
+	public:
+		     MinisipException( const std::string& what );
+		virtual ~MinisipException() throw();
+};
+
+class LIBMINISIP_API MinisipBadArgument : public MinisipException{
+	public:
+		MinisipBadArgument( const std::string& reason);
+		virtual ~MinisipBadArgument()throw();
+};
+
+#endif
