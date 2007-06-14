ab64d79c450c2de0c2776ec515fcb9e31422a7d8
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2007-06-14 15:03:28 +0000

    * Compile fix for libmnetutil when not configuring it with "--enable-ldap".
    git-svn-id: svn://svn.minisip.org/minisip/trunk@3323 63dae60f-c5d3-0310-92a3-fcbf00261c72

--- libmnetutil/source/Downloader.cxx
+++ libmnetutil/source/Downloader.cxx
@@ -1,16 +1,22 @@
 #include <libmnetutil/Downloader.h>
 #include <libmnetutil/HttpDownloader.h>
+
+#ifdef ENABLE_LDAP
 #include <libmnetutil/LdapDownloader.h>
+#endif
+
 #include <string>
 
 MRef<Downloader*> Downloader::create(std::string const uri) {
-	int pos = uri.find("://");
+	size_t pos = uri.find("://");
 	if (std::string::npos != pos) {
 		std::string protocol = uri.substr(0, pos);
 		if (protocol == "http")
 			return MRef<Downloader*>(dynamic_cast<Downloader*>(new HttpDownloader(uri)));
+#ifdef ENABLE_LDAP
 		else if (protocol == "ldap")
 			return MRef<Downloader*>(dynamic_cast<Downloader*>(new LdapDownloader(uri)));
+#endif
 	}
 	return MRef<Downloader*>();
 }
