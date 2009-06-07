1edd348651989de882973505b906b1490512336c
erik <erik@63dae60f-c5d3-0310-92a3-fcbf00261c72>
2009-06-07 14:24:56 +0000

    * Added missing source files (SipHeaderSnakeSM.{h,cxx}).
    git-svn-id: svn://svn.minisip.org/minisip/trunk@3659 63dae60f-c5d3-0310-92a3-fcbf00261c72

---
+++ libmsip/include/libmsip/SipHeaderSnakeSM.h
@@ -0,0 +1,53 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+
+/* Name
+ * 	SipHeaderSubject.h
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 
+*/
+
+#ifndef SIPHEADERSNAKESM_H
+#define SIPHEADERSNAKESM_H
+
+#include<libmsip/libmsip_config.h>
+
+#include<libmsip/SipHeaderString.h>
+
+/**
+ * @author Erik Eliasson
+*/
+
+extern SipHeaderFactoryFuncPtr sipHeaderSnakeSMFactory;
+
+class LIBMSIP_API SipHeaderValueSnakeSM: public SipHeaderValueString{
+	public:
+		SipHeaderValueSnakeSM(const std::string &build_from);
+
+                virtual std::string getMemObjectType() const {return "SipHeaderSnakeSM";}
+};
+
+#endif
---
+++ libmsip/source/headers/SipHeaderSnakeSM.cxx
@@ -0,0 +1,51 @@
+/*
+  Copyright (C) 2005, 2004 Erik Eliasson, Johan Bilien
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Authors: Erik Eliasson <eliasson@it.kth.se>
+ *          Johan Bilien <jobi@via.ecp.fr>
+*/
+
+
+/* Name
+ * 	SipHeaderValueSnakeSM.cxx
+ * Author
+ * 	Erik Eliasson, eliasson@it.kth.se
+ * Purpose
+ * 	This header is a non-default header used in the HDViper project
+*/
+
+#include<config.h>
+
+#include<libmsip/SipHeaderSnakeSM.h>
+
+using namespace std;
+
+MRef<SipHeaderValue *> snakeSMFactory(const string &build_from){
+	                return new SipHeaderValueSnakeSM(build_from);
+}
+
+SipHeaderFactoryFuncPtr sipHeaderSnakeSMFactory=snakeSMFactory;
+
+const string sipHeaderValueSnakeSMTypeStr = "Snake-SM";
+
+SipHeaderValueSnakeSM::SipHeaderValueSnakeSM(const string &build_from)
+		: SipHeaderValueString(SIP_HEADER_TYPE_SNAKESM,sipHeaderValueSnakeSMTypeStr, build_from)
+{
+}
+
